d50e331adb7b6f88f59dce129eca1d95874cb681
Ishan <ishanbhadauria@gmail.com>
2024-02-04 21:00:33 +0530

    first commit

---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { Button } from '@/components/ui/button';
+import { Menu } from 'lucide-react';
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+import Sidebar from '@/components/sidebar';
+import { useEffect, useState } from 'react';
+
+const MobileSidebar = () => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(()=>{
+        setIsMounted(true);
+    }, [])
+    if(!isMounted){
+        return null;
+    }
+  return (
+    <div>
+        <Sheet>
+        <SheetTrigger>
+        <Button variant = "ghost" size ="icon" className="md:hidden">
+        <Menu />
+      </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className='p-0'>
+        <Sidebar />
+      </SheetContent>
+      </Sheet>
+    </div>
+  )
+}
+
+export default MobileSidebar;
---
+++ components/navbar.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import MobileSidebar from "@/components/mobile-sidebar";
+
+const Navbar = async () => {
+
+  return ( 
+    <div className="flex items-center p-4">
+      <MobileSidebar />
+      <div className="flex w-full justify-end">
+        <UserButton afterSignOutUrl="/"/>
+      </div>
+    </div>
+   );
+}
+ 
+export default Navbar;
